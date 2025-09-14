236932109ecb175dc5f8a021c7a832b883eaa181
Diego Borba <diegoborbas03@gmail.com>
2025-09-14 15:14:55 -0300

    removendo start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Executa a aplicação Java, passando as propriedades de sistema.
-# O shell irá substituir as variáveis ($PGHOST, etc.) pelos seus valores.
-java -Dspring.datasource.url=jdbc:postgresql://${PGHOST}:${PGPORT}/${PGDATABASE} \
-     -Dspring.datasource.username=${PGUSER} \
-     -Dspring.datasource.password=${PGPASSWORD} \
-     -jar app.jar
